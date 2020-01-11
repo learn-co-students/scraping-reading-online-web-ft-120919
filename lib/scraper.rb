4bc6e501679c57f5b4dd9f21ecbfa9c2c24ebe8f
mrarthurwhite <mister.arthur.white@gmail.com>
2020-01-11 18:33:50 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+courses = doc.css(".gridContainer-EnvccM")
+#("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+#(".inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
+#(".copy-2raWRk")
+#(".gridContainer-EnvccM")
+
+#binding.pry
+
+courses.each do |course|
+  puts course.text.strip
+end
